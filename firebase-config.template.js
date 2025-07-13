751bf6c7d6a283ca288fdc6aa8c86068fb60f7f5
Marlow Eliot <101742018+mmmarlow@users.noreply.github.com>
2025-07-13 13:57:27 +0100

    Create firebase-config.template.js

---
+++ firebase-config.template.js
@@ -0,0 +1,11 @@
+// firebase-config.template.js
+// Copy this file to firebase-config.js and add your real values
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY_HERE",
+  authDomain: "YOUR_PROJECT.firebaseapp.com", 
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.firebasestorage.app",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
